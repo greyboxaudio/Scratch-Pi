1627867168e76a52c2bd5928954778d562fe235d
Robert <ra.vettel@outlook.com>
2025-08-01 08:23:21 +0200

    added xwax skin

--- 03_kernel.sh
+++ 03_kernel.sh
@@ -13,10 +13,10 @@ make -j6 Image.gz modules dtbs
 ENDTIME="$(date)"
 echo $STARTTIME
 echo $ENDTIME
-# sudo make -j6 modules_install
-# sudo cp /boot/firmware/$KERNEL.img /boot/firmware/$KERNEL-backup.img
-# sudo cp arch/arm64/boot/Image.gz /boot/firmware/$KERNEL.img
-# sudo cp arch/arm64/boot/dts/broadcom/*.dtb /boot/firmware/
-# sudo cp arch/arm64/boot/dts/overlays/*.dtb* /boot/firmware/overlays/
-# sudo cp arch/arm64/boot/dts/overlays/README /boot/firmware/overlays/
-# sudo reboot
+sudo make -j6 modules_install
+sudo cp /boot/firmware/$KERNEL.img /boot/firmware/$KERNEL-backup.img
+sudo cp arch/arm64/boot/Image.gz /boot/firmware/$KERNEL.img
+sudo cp arch/arm64/boot/dts/broadcom/*.dtb /boot/firmware/
+sudo cp arch/arm64/boot/dts/overlays/*.dtb* /boot/firmware/overlays/
+sudo cp arch/arm64/boot/dts/overlays/README /boot/firmware/overlays/
+sudo reboot
